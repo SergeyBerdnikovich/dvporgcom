b4225a79b0b6928cf1b91d3d6e6eb89e9e429e28
SergeyBerdnikovich <atom@tut.by>
2013-04-24 12:40:30 +0300

    Initial Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dvporgcom::Application.config.secret_token = 'f523d4e007780b7b2606fedddc891cde77d54bbd3a3a9782a6954952c5af95b9ac1462f35ecc4be280b0d5ad2478cc9e7ee6de1382efc4e316386c1baf13f869'
